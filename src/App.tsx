e9433fe5ab5a22ca98adc3ff4994bc170f909576
Simranjeet Singh <smrnjeet222@gmail.com>
2021-07-15 13:20:32 +0530

    erc20 token

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@
 
 import React, { useState } from "react";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import Token from "./artifacts/contracts/Token.sol/Token.json";
 import { ethers } from "ethers";
 
 // Update with the contract address logged out to the CLI when it was deployed
-const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const tokenAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 declare let window: any;
 
 function App() {
   // store greeting in local state
   const [greeting, setGreetingValue] = useState("");
+
+  const [userAccount, setUserAccount] = useState("");
+  const [amount, setAmount] = useState("");
 
   // request access to the user's MetaMask account
   async function requestAccount() {
@@ -48,6 +53,31 @@ function App() {
       fetchGreeting();
     }
   }
+  // tranfer token
+  async function sendCoins() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transation = await contract.transfer(userAccount, amount);
+      await transation.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
+  // get balance
+  async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+      const [account] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
 
   return (
     <div className='App'>
@@ -57,6 +87,18 @@ function App() {
         <input
           onChange={(e) => setGreetingValue(e.target.value)}
           placeholder='Set greeting'
+        />
+
+        <br />
+        <button onClick={getBalance}>Get Balance</button>
+        <button onClick={sendCoins}>Send Coins</button>
+        <input
+          onChange={(e) => setUserAccount(e.target.value)}
+          placeholder='Account ID'
+        />
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder='Amount'
         />
       </header>
     </div>
